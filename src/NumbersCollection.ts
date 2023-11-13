769a6087851ec7aa6be005d0cca826049c1ceada
Maysummer <udojyk@gmail.com>
2023-11-13 19:55:46 +0100

    refactor class to extract functionality

---
+++ src/NumbersCollection.ts
@@ -0,0 +1,18 @@
+export class NumbersCollection {
+  constructor(public data: number[]) {}
+
+  //get allows you to access the method without calling it like a method i.e. without parentheses
+  get length(): number {
+    return this.data.length;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex]
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const temp = this.data[leftIndex];
+      this.data[leftIndex] = this.data[rightIndex];
+      this.data[rightIndex] = temp;
+  }
+}
---
+++ src/Sorter.ts
@@ -0,0 +1,25 @@
+import { NumbersCollection } from "./NumbersCollection";
+
+export class Sorter {
+  //long method
+  /*collection: number[];
+
+  constructor(collection: number[]) {
+    this.collection = collection;
+  }*/
+
+  //shorter method by using a modifier inside the constructor argument
+  constructor(public collection: NumbersCollection) {}
+
+  sort() :void {
+    const {length} = this.collection;
+
+    for (let i=0; i < length; i++) {
+      for (let j=0; j < length - i - 1; j++) {
+        if (this.collection.compare(j, j+1)) {
+          this.collection.swap(j, j+1);
+        }
+      }
+    }
+  }
+}
